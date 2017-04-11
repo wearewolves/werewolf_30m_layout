ec6ccc6b75cdb0ba52b2ba3a0000b3aae2aa3ab4
kijuli <rekenber@gmail.com>
2017-04-12 03:08:36 +0900

    gulpfile.js 추가

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2));
+gulp.task('deploy', function() {
+  var remotePath = '/www/';
+  console.log("deploy " + args.branch+" "+args.tag)
+  var conn = ftp.create({
+    host: 'werewolf6.cafe24.com',
+    user: args.user,
+    password: args.password,
+    log: gutil.log,
+    maxConnections: 4
+  });
+  gulp.src(['README.md'])
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath));
+  gulp.src(['Werewolf/**/*.*'])
+    .pipe(conn.newer(remotePath + 'Werewolf/'))
+    .pipe(conn.dest(remotePath + 'Werewolf/'));
+});
+
